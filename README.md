c45711187ac4a8cab800207c5a8cf0e6e4064358
Martin Schade <schadix@gmail.com>
2015-08-05 21:29:51 -0400

    basic working version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# NodeTweetStream2Kinesis
-NodeTweetStream2Kinesis
+config.json format:
+
+{
+	"kinesis_stream": <stream_name>,
+	"region": <region e.g. us-east-1,
+	"twitter_keys": {
+		"consumer_key": <consumer_key>
+		"consumer_secret": <consumer_secret>,
+		"access_token_key": <access_token_key>,
+		"access_token_secret": <access_token_secret>
+	}
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+var Twitter = require('twitter');
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+var nconf = require('nconf');
+
+nconf.argv()
+	.env()
+	.file({
+		file: './config.json'
+	});
+nconf.load();
+
+var streamName = nconf.get('kinesis_stream');
+var twitterClient = new Twitter(nconf.get('twitter_keys'));
+AWS.config.update({
+	region: nconf.get('region')
+});
+
+var kinesis = new AWS.Kinesis();
+
+twitterClient.stream('statuses/filter', {
+	locations: "-180,-90,180,90"
+}, function(stream) {
+	stream.on('data', function(tweet) {
+		var params = {
+			Data: JSON.stringify(tweet),
+			PartitionKey: uuid.v4(),
+			StreamName: streamName
+		};
+		kinesis.putRecord(params, function(err, data) {
+			if (err) console.log(err, err.stack);
+			else console.log(data);
+		});
+	});
+
+	stream.on('error', function(error) {
+		// console.error(error);
+	});
+});
